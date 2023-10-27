7a7da8e912f304b96316e3a146638caf22687cf1
emran <shuvoorba28@gmail.com>
2023-10-27 09:32:53 +0600

    img not found problem has solved

--- index.js
+++ index.js
@@ -44,17 +44,25 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const options = {
                 // Include only the `title` and `imdb` fields in the returned document
-                projection: {title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
 
             const result = await serviceCollection.findOne(query, options)
             res.send(result)
         })
 
-        
+        app.get('/checkouts', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await checkOutCollection.find(query).toArray();
+            res.send(result)
+        })
 
-        app.post('/checkouts', async(req, res) => {
-            const checkOut = req.body; 
+        app.post('/checkouts', async (req, res) => {
+            const checkOut = req.body;
             console.log(checkOut);
             const result = await checkOutCollection.insertOne(checkOut)
             res.send(result)
